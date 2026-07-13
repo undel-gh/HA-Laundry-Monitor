b7f8f02ac86dae280c7779359773707d70ec3eca
undel-gh <dnesterkin@gmail.com>
2026-07-13 18:22:26 +0300

    Update entity.py

--- custom_components/laundry_monitor/entity.py
+++ custom_components/laundry_monitor/entity.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from homeassistant.core import callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import Entity
@@ -34,6 +35,7 @@ class LaundryMonitorEntity(Entity):
     async def async_added_to_hass(self) -> None:
         """Subscribe to runtime updates."""
         await super().async_added_to_hass()
+
         self.async_on_remove(
             async_dispatcher_connect(
                 self.hass,
@@ -42,6 +44,7 @@ class LaundryMonitorEntity(Entity):
             )
         )
 
+    @callback
     def _handle_runtime_update(self) -> None:
         """Write the latest runtime value to Home Assistant."""
         self.async_write_ha_state()
